2d78de17abd1659b375e452863a803102a67b283
gerardo <pardocz@gmail.com>
2018-05-12 16:17:59 +0200

    Do not show alerts for types that are not recognized, but allow new types to be declared dynamically when using the helper.

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -7,11 +7,13 @@ module BootstrapFlashHelper
     warning: :warning
   }.freeze
 
-  def bootstrap_flash
+  def bootstrap_flash(additional_accepted_types={})
     safe_join(flash.each_with_object([]) do |(type, message), messages|
                 next if message.blank? || !message.respond_to?(:to_str)
-                type = ALERT_TYPES_MAP.fetch(type.to_sym, type)
-                messages << flash_container(type, message)
+                type = ALERT_TYPES_MAP.merge(additional_accepted_types).fetch(type.to_sym, type)
+                if type
+                  messages << flash_container(type, message)
+                end
               end, "\n").presence
   end
 
